025f191375b8e006734da5ceafb2c32200ff5e3b
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-11 10:10:04 -0500

    ES5 way of looping around a list by making it an slicing into an array.

--- examples/script.js
+++ examples/script.js
@@ -1,4 +1,6 @@
 const boxes = document.querySelectorAll(".box");
+//recall that querySelectorAll returns a node list
+//we want to make that node list into a node array so we can loop through them with forEach
 
 //ES5
 var boxesArr = Array.prototype.slice.call(boxes);
